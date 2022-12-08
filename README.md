49f6cf442587d257f01ac10c7a7a2b86f25ac26c
antran1245 <antran1245@gmail.com>
2022-12-07 20:15:02 -0800

    determine the strength  by checkbox and  slider

--- README.md
+++ README.md
@@ -2,6 +2,70 @@
 
 Create a way for users to generate password with certain requirement, such as captial letter or a symbol.
 
+## Password Strength
+
+<div style="display: flex">
+    <div style="width: 50%">
+        <b style="color:#A4FFAF">STRONG</b>
+        <ul>
+            <li>Length greater than 15</li>
+            <li>Uppercase</li>
+            <li>Lowercase</li>
+            <li>Numbers</li>
+            <li>Symbols</li>
+        </ul>
+    </div>
+    <div style="width: 50%">
+        <b style="color:#F8CD65">MEDIUM</b>
+        <ul>
+            <li>Length greater than 15</li>
+            <li>At least 3 of the following:</li>
+            <ul>
+                <li>Uppercase</li>
+                <li>Lowercase</li>
+                <li>Numbers</li>
+                <li>Symbols</li>
+            </ul>
+        </ul>
+    </div>
+</div>
+<div style="display: flex">
+    <div style="width: 50%">
+        <b style="color:#FB7C58">WEAK</b>
+        <ul>
+            <li>Length greater than 8 </li>
+            <li>Uppercase</li>
+            <li>Lowercase</li>
+            <li>Numbers</li>
+            <li>Symbols</li>
+        </ul>
+        <p>OR</p>
+        <ul>
+            <li>Length greater than 15</li>
+            <li>At least 2 of the following:</li>
+            <ul>
+                <li>Uppercase</li>
+                <li>Lowercase</li>
+                <li>Numbers</li>
+                <li>Symbols</li>
+            </ul>
+        </ul>
+    </div>
+    <div style="width: 50%">
+        <b style="color:#F64A4A">TOO WEAK!</b>
+        <ul>
+            <li>Any Length</li>
+            <li>With only one of the following:</li>
+            <ul>
+                <li>Uppercase</li>
+                <li>Lowercase</li>
+                <li>Numbers</li>
+                <li>Symbols</li>
+            </ul>
+        </ul>
+    </div>
+</div>
+
 # Packages
 - TypeScript
 - Tailwind CSS
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import copy from './assets/images/icon-copy.svg'
 import arrow from './assets/images/icon-arrow-right.svg'
 import './App.css';
 
 function App() {
+  const [slideNum, setSlideNum] = useState<number>(10)
+  const [level, setLevel] = useState<string>('TOO WEAK!')
+  const [bar, setBar] = useState<number>(1)
+  const [include, setInclude] = useState<{uppercase: boolean, lowercase: boolean, numbers: boolean, symbols: boolean} | any>({uppercase: false, lowercase: false, numbers: false, symbols: false})
+  
+  useEffect(() => {
+    // Determine the strength of the passwword
+    let strength = 0
+    if (slideNum >= 16) {
+      strength++
+    } else if (slideNum < 16 && slideNum >= 8) {
+      strength -= 1
+    } else if (slideNum < 8) {
+      strength -= 2
+    }
+    for(const key in include) {
+      if(include[key]) {
+        strength++
+      }
+    }
+    // Set the number of bar and message
+    if(strength <= 2) {
+      setLevel('TOO WEAK!')
+      setBar(1)
+    }
+    if(strength === 3) {
+      setLevel('WEAK')
+      setBar(2)
+    }
+    if(strength === 4) {
+      setLevel('MEDIUM')
+      setBar(3)
+    }
+    if(strength === 5) {
+      setLevel('STRONG')
+      setBar(4)
+    }
+  }, [slideNum, include])
+  
   return (
     <main className="m-auto">
       <p className='text-grey heading-medium text-center mb-[31px] '>Password Generator</p>
@@ -20,30 +59,30 @@ function App() {
         {/* Choose the length of the password to generate */}
         <div className='flex justify-between items-center'>
           <p className='text-white body'>Character Length</p>
-          <p className='text-green heading-large'>10</p>
+          <p className='text-green heading-large'>{slideNum}</p>
         </div>
         <input type="range" 
-          max={32} min={1} defaultValue={10} 
-          onChange={(e) => console.log(e.target.value)}
+          max={32} min={1} value={slideNum} 
+          onChange={(e) => setSlideNum(Number(e.target.value))}
           className="slider cursor-pointer w-full mt-[16px]" 
         />
 
         {/* Checkboxes section that let users choose what to include in their password */}
         <div className='my-[32px]'>
           <label className='form-control'>
-            <input type="checkbox" />
+            <input type="checkbox" onChange={(e) => setInclude({...include, uppercase: e.target.checked})}/>
             <p className='text-white body ml-[24px]'>Include Uppercase Letters</p>
           </label>
           <label className='form-control mt-[20px]'>
-            <input type="checkbox" />
+            <input type="checkbox" onChange={(e) => setInclude({...include, lowercase: e.target.checked})}/>
             <p className='text-white body ml-[24px]'>Include Lowercase Letters</p>
           </label>
           <label className='form-control mt-[20px]'>
-            <input type="checkbox" />
+            <input type="checkbox" onChange={(e) => setInclude({...include, numbers: e.target.checked})}/>
             <p className='text-white body ml-[24px]'>Include Numbers</p>
           </label>
           <label className='form-control mt-[20px]'>
-            <input type="checkbox" />
+            <input type="checkbox" onChange={(e) => setInclude({...include, symbols: e.target.checked})}/>
             <p className='text-white body ml-[24px]'>Include Symbols</p>
           </label>
         </div>
@@ -52,13 +91,13 @@ function App() {
         <div className='bg-black py-[20px] px-[32px] sm:py-[14px] sm:px-[16px] flex items-center justify-between'>
           <p className='text-grey body'>STRENGTH</p>
           <div className='flex items-center'>
-            <p className='text-white heading-medium mr-[16px]'>MEDIUM</p>
+            <p className='text-white heading-medium mr-[16px]'>{level}</p>
             {/* Power bars */}
             <div className='boxes flex items-center'>
-              <div className={`box bg-yellow`}></div>
-              <div className={`box bg-yellow ml-[8px]`}></div>
-              <div className={`box bg-yellow ml-[8px]`}></div>
-              <div className={`box ml-[8px] border-[#E6E5EA] border-[2px]`}></div>
+              <div className={`box ${bar === 0? 'border-[#E6E5EA] border-[2px]' : ''} ${bar === 1?'bg-red' : ''} ${bar === 2?'bg-orange' : ''} ${bar === 3?'bg-yellow' : ''} ${bar === 4?'bg-green' : ''}`}></div>
+              <div className={`box ml-[8px] ${bar <= 1? 'border-[#E6E5EA] border-[2px]' : ''} ${bar === 2?'bg-orange' : ''} ${bar === 3?'bg-yellow' : ''} ${bar === 4?'bg-green' : ''}`}></div>
+              <div className={`box ml-[8px] ${bar <= 2? 'border-[#E6E5EA] border-[2px]' : ''} ${bar === 3?'bg-yellow' : ''} ${bar === 4?'bg-green' : ''}`}></div>
+              <div className={`box ml-[8px] ${bar <= 3? 'border-[#E6E5EA] border-[2px]' : ''} ${bar === 4?'bg-green' : ''}`}></div>
             </div>
           </div>
         </div>
